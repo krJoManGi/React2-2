937def83de93eedf14a8ba25215c10df675b2323
krJoManGi <162421541+krJoManGi@users.noreply.github.com>
2024-11-06 12:37:56 +0900

    1106

---
+++ next14-ui/src/app/tailwind/page.js
@@ -0,0 +1,10 @@
+export default function TailWind() {
+    return (
+        <>
+            <h1>TailWind Page.</h1>
+            <h1 class="text-3xl font-bold underline">
+                Hello World
+            </h1>
+        </>
+    )
+}
